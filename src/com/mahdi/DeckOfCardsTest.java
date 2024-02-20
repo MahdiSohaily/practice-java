5371b70457494116b3f5a04870f34fec3beb0575
MahdiSohaily <mahdi.sohaily4030@gmail.com>
2024-02-20 11:38:31 +0330

    Refactor DeckOfCardsTest.java

--- src/com/mahdi/DeckOfCardsTest.java
+++ src/com/mahdi/DeckOfCardsTest.java
@@ -1,12 +1,9 @@
 package com.mahdi;
 
-import java.time.Month;
-
 public class DeckOfCardsTest {
 
-    public static void main (String[] args)
-    {
-        System.out.println(Math.max(3,6));
-        Month current = Month.MARCH;
+    public static void main(String[] args) {
+        System.out.println("Array".indexOf("r"));
+        System.out.println("Mahdi Rezaei".substring(0,5));
     }
 }
