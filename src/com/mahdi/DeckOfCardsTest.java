a1c13b2bf687ad4fe6c09b51fd2f2a84e33f2077
MahdiSohaily <mahdi.sohaily4030@gmail.com>
2024-02-21 12:04:22 +0330

    practice java string methods

--- src/com/mahdi/DeckOfCardsTest.java
+++ src/com/mahdi/DeckOfCardsTest.java
@@ -1,10 +1,16 @@
 package com.mahdi;
 
+import java.util.Arrays;
+
 public class DeckOfCardsTest {
 
     public static void main(String[] args) {
-        System.out.println("Array".indexOf("r"));
-        System.out.println("Mahdi Rezaei".substring(0,5));
+        System.out.println("Mahdi".endsWith("i"));
+        System.out.println("Mahdi".equals("mahdi"));
+        System.out.println(String.format("Mahdi %d", 3));
+
+        System.out.println("Mahdi Rezaei Rezaei".replaceFirst("Rezaei","Sohaily" ));
+        System.out.println(Arrays.toString("Mahdi Rezaei Sohaily".split(" ")));
 
     }
 }
