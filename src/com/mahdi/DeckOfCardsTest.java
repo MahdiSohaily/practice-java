744c19da4ffa7a31348d49d5ed572bca5fb2526d
MahdiSohaily <mahdi.sohaily4030@gmail.com>
2024-02-22 12:15:40 +0330

    Practice the array functions and creating different array types

--- src/com/mahdi/DeckOfCardsTest.java
+++ src/com/mahdi/DeckOfCardsTest.java
@@ -3,12 +3,9 @@ package com.mahdi;
 public class DeckOfCardsTest {
 
     public static void main(String[] args) {
-        double x = 1.0;
-        double y = 2.0;
-
-        // Get the floating-point number adjacent to x in the direction of y
-        double result = Math.nextAfter(x, y);
-
-        System.out.println("Next after " + x + " in the direction of " + y + " is: " + result);
+        char[] names = new char[10];
+        for (char name : names) {
+            System.out.println(name);
+        }
     }
 }
