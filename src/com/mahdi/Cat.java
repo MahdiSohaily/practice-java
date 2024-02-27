e55b9b6a1d30d4b6dcdafdaa93bae1c2a2a45a87
MahdiSohaily <mahdi.sohaily4030@gmail.com>
2024-02-27 14:22:28 +0330

    set the getter and setter and the

--- src/com/mahdi/Cat.java
+++ src/com/mahdi/Cat.java
@@ -5,4 +5,46 @@ public class Cat {
     String color;
     int age;
 
+    public Cat() {
+    }
+
+    public Cat(String name, String color, int age) {
+        this.name = name;
+        this.color = color;
+        this.age = age;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    @Override
+    public String toString() {
+        return "Cat{" +
+                "name='" + name + '\'' +
+                ", color='" + color + '\'' +
+                ", age=" + age +
+                '}';
+    }
+
 }
