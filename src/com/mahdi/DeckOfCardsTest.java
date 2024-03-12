0c0e91c485d4e20f4004e8c14d53895a11af6ee4
MahdiSohaily <mahdi.sohaily4030@gmail.com>
2024-03-12 19:29:30 +0330

    practice problem solving in Strings

--- src/com/mahdi/DeckOfCardsTest.java
+++ src/com/mahdi/DeckOfCardsTest.java
@@ -1,21 +1,20 @@
 package com.mahdi;
 
 public class DeckOfCardsTest {
-
     public static void main(String[] args) {
-        greeting();
-        DeckOfCardsTest test = new DeckOfCardsTest();
-        test.welcome();
+//        findAbc("abcd");
+        findAbc("abcabcabcabca");
     }
-    public static void greeting () {
-        System.out.println("Hello dear Mahdi Rezaei");
-    }
-    public void welcome() {
-        System.out.println("Welcome to the Access Modifiers tutorial");
+    public static void findAbc(String input) {
+        int index = input.indexOf("abc");
+        while (true){
+            if (index == -1 || index >= input.length() - 3){
+                break;
+            }
+            String found = input.substring(index+1, index+4);
+            System.out.println(found);
+            index = input.indexOf("abc",index+3);
+        }
     }
 }
 
-abstract  class Test  {
-    String name = "Mahdi";
-    
-}
